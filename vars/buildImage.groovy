75de7bddc2ce3d7a6326f1d69d7fd3764eaed30f
Saurabh Bisht <59279947+saurabh277@users.noreply.github.com>
2023-04-26 13:19:49 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credential', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t saurabh277/jenkinsapp:3.0 .'
+        sh 'docker build -t saurabh277/jenkinsapp .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push saurabh277/jenkinsapp:3.0'
+        sh 'docker push saurabh277/jenkinsapp'
     }
 }
